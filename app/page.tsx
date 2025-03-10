4701e0f7f8e330dc4784a7e99804e96d5caf3426
0xsid0703 <daniel73matthew1213@gmail.com>
2025-03-09 21:21:07 -0400

    added

--- app/page.tsx
+++ app/page.tsx
@@ -5,42 +5,12 @@ import useSWR from 'swr'
 
 export default function Home() {
   const { data, error, isLoading } = useSWR('/api/getAllScore', fetcher);
-  const setClick = async () => {
-    const response = await axios.post("/api/setLatestProtein", {
-      protein: "ERERE"
-    })
-    console.log("Set response", response.data)
-  }
   const getClick = async () => {
     const response = await axios.get('/api/getLatestProtein')
     console.log("Get Response: ", response.data)
   }
 
-  const setScoreClick = async () => {
-    const response = await axios.post("/api/setLatestScore", {
-      protein: "PDSD89D",
-      product: "NDFOU090890*DFD)sdfj_fsdf",
-      score: 10.957445
-    })
-    console.log("Set Score response", response.data)
-  }
-  const setHigherScoreClick = async () => {
-    const response = await axios.post("/api/setLatestScore", {
-      protein: "PDSD89D",
-      product: "NDFOU090890*DFD)sdfj_fsdf",
-      score: 8.957445
-    })
-    console.log("Set Higher Score response", response.data)
-  }
-  const setLowerScoreClick = async () => {
-    const response = await axios.post("/api/setLatestScore", {
-      protein: "PDSD89D",
-      product: "NDFOU090890*DFD)sdfj_fsdf",
-      score: 11.957445
-    })
-    console.log("Set Lower Score response", response.data)
-  }
-  const getScoreClick = async () => {
+    const getScoreClick = async () => {
     const response = await axios.post('/api/getLatestScore', { protein: "PDSD89D" })
     console.log("Get Score Response: ", response.data)
   }
@@ -50,11 +20,7 @@ export default function Home() {
     console.log({ data })
     return (
       <div className="mx-5 md:mx-10 flex flex-col gap-2">
-        <button onClick={setClick}>Set Latest Protein</button>
         <button onClick={getClick}>Get Latest Protein</button>
-        <button onClick={setScoreClick}>Set Latest Score</button>
-        <button onClick={setHigherScoreClick}>Set Higher Latest Score</button>
-        <button onClick={setLowerScoreClick}>Set Lower Latest Score</button>
         <button onClick={getScoreClick}>Get Latest Score</button>
         <div className="w-full">
           <table className='w-full table-auto'>
