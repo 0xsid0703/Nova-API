cc9041ec5f163caedf7ae64280534fdde5802511
0xsid0703 <daniel73matthew1213@gmail.com>
2025-03-30 22:18:52 -0400

    added

--- app/api/getPrice/route.ts
+++ app/api/getPrice/route.ts
@@ -1,23 +1,50 @@
 import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+import axios from "axios";
 export const dynamic = 'force-dynamic'
 export const revalidate = 0;
+type EXPORT_DATA = {
+  name: string;
+  price: string;
+}
 export async function GET() {
-    try {
-        const response = await fetch("https://api.mexc.com/api/v3/ticker/price", {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            cache: "no-cache"
-          });
-      
-          if (!response.ok) {
-            return NextResponse.json({ error: "Failed to fetch data" });
-          }
-      
-          const data = await response.json();
-          return NextResponse.json(data);
-    } catch (error) {
-        return NextResponse.json({ error: (error as Error).message || "Internal Server Error" });
+  try {
+    const subnets = await prisma.subnets.findMany({ orderBy: { subnet: 'asc' } });
+    const mysubnets = subnets.map((subnet: any) => subnet.subnet);
+    const response = await fetch("https://api.mexc.com/api/v3/ticker/price", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      cache: "no-cache"
+    });
+
+    if (!response.ok) {
+      return NextResponse.json({ error: "Failed to fetch data" });
     }
+    const data = await response.json();
+    const taoPrice = data.find((price: { symbol: string; }) => price.symbol === 'TAOUSDT').price
+    const btcPrice = data.find((price: { symbol: string; }) => price.symbol === 'BTCUSDT').price
+    const response_data: EXPORT_DATA[] = [
+      {
+        name: 'TAO',
+        price: taoPrice
+      },
+      {
+        name: 'BTC',
+        price: btcPrice
+      }
+    ]
+    for (const subnet_uid of mysubnets) {
+      const taox_api = await axios.post(`https://taoxnet.io/api/v1/netuid/netinfo?network=mainnet`, { netuid: subnet_uid })
+      const price = await taox_api.data
+      response_data.push({
+        name: `Subnet ${subnet_uid}`,
+        price: price.price
+      })
+    }
+    return NextResponse.json(response_data);
+  } catch (error) {
+    return NextResponse.json({ error: (error as Error).message || "Internal Server Error" });
+  }
 }
---
+++ components/PriceBar.tsx
@@ -0,0 +1,27 @@
+"use client"
+import { fetcher } from '@/utils/fetcher'
+import React from 'react'
+import useSWR from 'swr'
+import { showNumber } from '@/lib/main'
+
+const PriceBar = () => {
+    const { data, error, isLoading } = useSWR('/api/getPrice', fetcher)
+    if (isLoading) return <div className='w-full h-full'>
+        ...
+    </div>
+    if (error) return <div className='w-full h-full flex flex-col gap-3 items-center justify-center'>
+        <img src="/mark.png" className='w-32 h-24' alt='' />
+        Data Fetching Error
+    </div>
+    if (data) {
+        return (
+            <div className='w-full flex flex-col gap-1 items-center text-sm'>
+                {data && data.length > 0 && data.map((item: any) => (
+                    <span key={item.name}>{item.name}: ${showNumber(item.price, 5)}</span>
+                ))}
+            </div>
+        )
+    }
+}
+
+export default PriceBar
